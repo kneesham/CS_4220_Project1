dd92ff9a5aa341185e700ea77cab398b86c9b231
Kneesham <vlog.car.guy@gmail.com>
2020-06-25 10:50:43 -0500

    Adding files from project 1

---
+++ EXTRACREDIT.md
@@ -0,0 +1,3 @@
+#  EXTRACREDIT OVERVIEW
+For the extra credit part of this project, I did some very simple stuff. Basically I just changed the background color of the app and the font as a different one than the original and that is about it.
+
---
+++ ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  TedNeshamAboutMe
+//
+//  Created by Ted Nesham on 6/20/20.
+//  Copyright © 2020 umsl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var pictureOfMe: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
